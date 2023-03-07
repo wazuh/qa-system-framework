4d1c54744f939b8d3fde53c4b0ff03cf3fc84740
jmv74211 <jmv74211@gmail.com>
2023-03-07 18:09:48 +0100

    feat(#9): add new test utils functions

--- src/wazuh_qa_framework/meta_testing/utils.py
+++ src/wazuh_qa_framework/meta_testing/utils.py
@@ -3,6 +3,9 @@ Util functions to use in the framework testing.
 """
 
 import re
+import os
+import sys
+import logging
 
 CUSTOM_REGEX = r'.*wazuh-modulesd:aws-s3: INFO: Executing Service Analysis'
 DEFAULT_LOG_MESSAGE = '2023/02/14 09:49:47 wazuh-modulesd:aws-s3: INFO: Executing Service Analysis\n'
@@ -40,3 +43,29 @@ def append_log(file, content):
     """
     with open(file, 'a') as _file:
         _file.write(content)
+
+
+def write_file(file, content=''):
+    """Write content to file.
+
+    Args:
+        file (str): File path.
+        content (str): Content to write.
+    """
+    with open(file, 'w') as _file:
+        _file.write(content)
+
+
+def remove_file(file):
+    """Remove a file if exists.
+
+    Args:
+        file (str): File path.
+    """
+    if os.path.exists(file) and os.path.isfile(file):
+        if sys.platform == 'win32':
+            # Shutdown logging. Needed because on Windows we can't remove the logging file if the file handler is set
+            # and up.
+            logging.shutdown()
+
+        os.remove(file)
