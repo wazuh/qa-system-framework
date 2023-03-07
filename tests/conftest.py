aacb3082eacb4a97202c302bd01f44fcd5796db3
jmv74211 <jmv74211@gmail.com>
2023-03-07 18:11:29 +0100

    refactor(#9): update generic test conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,20 +2,28 @@ import pytest
 import os
 from tempfile import gettempdir
 
+from wazuh_qa_framework.meta_testing.utils import write_file, remove_file
+
 DEFAULT_SAMPLE_FILE = os.path.join(gettempdir(), 'file.log')
 
 
 @pytest.fixture
 def create_destroy_sample_file(request):
     """Create and destroy a sample file"""
-    file = request.module['SAMPLE_FILE'] if hasattr(request.module, 'SAMPLE_FILE') else DEFAULT_SAMPLE_FILE
-
-    # Create an empty file
-    with open(file, 'a'):
-        pass
+    file = getattr(request.module, 'SAMPLE_FILE') if hasattr(request.module, 'SAMPLE_FILE') else DEFAULT_SAMPLE_FILE
+    write_file(file)
 
     yield file
 
-    # Remove the file
-    if os.path.exists(file):
-        os.remove(file)
+    remove_file(file)
+
+
+@pytest.fixture
+def create_destroy_sample_file_with_content(request, file_content):
+    """Create and destroy a sample file with specified content"""
+    file = getattr(request.module, 'SAMPLE_FILE') if hasattr(request.module, 'SAMPLE_FILE') else DEFAULT_SAMPLE_FILE
+    write_file(file, content=file_content)
+
+    yield file
+
+    remove_file(file)
