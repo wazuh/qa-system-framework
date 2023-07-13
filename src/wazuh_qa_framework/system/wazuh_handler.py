493d7d09a410dcfad855800110069be3e4544ff7
Antonio <antonio.kim@wazuh.com>
2023-07-13 10:35:36 +0200

    fix(#66): fixing whitespaces after linting

--- src/wazuh_qa_framework/system/wazuh_handler.py
+++ src/wazuh_qa_framework/system/wazuh_handler.py
@@ -861,7 +861,6 @@ class WazuhEnvironmentHandler(HostManager):
             else:
                 current_rules = self.get_file_content(host, rules_filename)
                 index_rule = current_rules.rfind("</rule>")
-
                 if index_rule != -1:
                     new_rules = current_rules[:index_rule] + '</rule>\n' +  new_rules + '\n</group>'
                     self.logger.info(message=f'Adding rule from {new_rule_filename} to {rules_filename}')
@@ -890,7 +889,6 @@ class WazuhEnvironmentHandler(HostManager):
             else:
                 current_decoders = self.get_file_content(host, decoder_filename)
                 index_decoder = current_decoders.rfind("</decoder>")
-
                 if index_decoder != -1:
                     new_decoders = current_decoders[:index_decoder] + '</decoder>\n' + new_decoders
                     self.logger.info(message=f'Adding decoder from {new_decoder_filename} to {decoder_filename}')
